b02f83a3fd3ab0331541921957109a9b2f905c0f
Alejandro Lozano <alexlozanob@gmail.com>
2017-05-05 14:41:32 -0500

    Events and callbacks, Closes #8

--- public/app.jsx
+++ public/app.jsx
@@ -5,13 +5,35 @@ var Greeter = React.createClass({
         message: 'This is from a component.'
       };
   },
+  getInitialState: function(){
+    return {
+      name: this.props.name
+    };
+  },
+  onButtonClick: function(e){
+    e.preventDefault();
+
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value = '';
+
+    if (typeof name === 'string' && name.length > 0){
+      this.setState({
+        name: name
+      });
+    }
+  },
   render: function (name){
-    var name = this.props.name
+    var name = this.state.name
     var message = this.props.message
     return (
       <div>
         <h1>Hello {name}!!!</h1>
         <p>{message}</p>
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
